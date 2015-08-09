e8d641dd49403a8eaeb6875dc31073963e04c234
oceanborn-mx <da.razo@yahoo.com>
2015-08-09 17:33:43 -0500

    major tweaks, added swap function

--- src/fft.cpp
+++ src/fft.cpp
@@ -1,23 +1,33 @@
-// ******************************************************************************
-// * File: fft.cpp                                                              *
-// *                                                                            *
-// * Description: Program to perform the FFT algorithm                          *
-// *                                                                            *
-// * Author: Daniel A Razo M                                                    *
-// *                                                                            *
-// * email: da.razo@yahoo.com                                                   *
-// ******************************************************************************
+///////////////////////////////////////////////////////////////////////////////
+//    FFT: Fast Fourier Transform                                             
+//    Copyright (C) 2015  Daniel A Razo M (da.razo@yahoo.com)             
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+///////////////////////////////////////////////////////////////////////////////
 
 // TODO: * clean-up the code.
 //       * make generic abount the number of FFT points, always in power of 2
 //       * remove the hard-coded values.
-//       * implement a >> SWAP << function in situ, because of the bit reversal
-//         algorithm nature.
+//       * try to optimize the ** SWAP ** function.
+//       * use c++ 14 initializers, constructors, etc.
 //       * your contribution is welcome
 
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <iostream>
+#include <iomanip>
 
 #define PI      M_PI        // pi to machine precision, defined in math.h
 #define TWOPI   (2.0 * PI)
@@ -87,7 +97,9 @@ void fourier(COMPLEX_NUMBER data[], int nn, int isign)
 	wr = 1.0;
 	wi = 0.0;
 
+#ifdef _DEBUG_
     cout << "NSTAGES: " << NSTAGES << endl;
+#endif
 
     for (int i = 0; i < NSTAGES; ++i)
     {
@@ -95,7 +107,7 @@ void fourier(COMPLEX_NUMBER data[], int nn, int isign)
         k = 0;
 
 #ifdef _DEBUG_ 
-        cout << "### i: " << i << endl;
+        //cout << "### i: " << i << endl;
 #endif
         for (int j = 0; j < NN - 1; j += 2)
         {
@@ -114,39 +126,40 @@ void fourier(COMPLEX_NUMBER data[], int nn, int isign)
             u.bits = j;
             v.bits = j + 1;
 
-            if (0 == i) // auxiliar counter initialization
+            // auxiliar counter for **swap** function
+            // initialize
+            if (0 == i)
             {
                 uaux.bits = u.bits;
                 vaux.bits = v.bits;
                 array[j].bits = u.bits;
                 array[j + 1].bits = v.bits;
             }
-            //else        // hold the actual value
-            //{
-                uaux.bits = uaux.bits;
-                vaux.bits = vaux.bits;
-                array[j].bits = array[j].bits;
-                array[j + 1].bits = array[j + 1].bits;
+            
+            // hold the actual value
+            uaux.bits = uaux.bits;
+            vaux.bits = vaux.bits;
+            array[j].bits = array[j].bits;
+            array[j + 1].bits = array[j + 1].bits;
 
-                a.bits = array[j].bits & 0x1;
-                b.bits = array[j].bits & (0xF >> i);
-                c.bits = array[j].bits & ((~0xF) >> i);
-                x.bits = a.bits << (NSTAGES - 1 - i);
-                y.bits = b.bits >> 1;
+            a.bits = array[j].bits & 0x1;
+            b.bits = array[j].bits & (0xF >> i);
+            c.bits = array[j].bits & ((~0xF) >> i);
+            x.bits = a.bits << (NSTAGES - 1 - i);
+            y.bits = b.bits >> 1;
 
-                aa.bits = array[j+1].bits & 0x1;
-                bb.bits = array[j+1].bits & (0xF >> i);
-                cc.bits = array[j+1].bits & ((~0xF) >> i);
-                xx.bits = aa.bits << (NSTAGES - 1 - i);
-                yy.bits = bb.bits >> 1;
+            aa.bits = array[j+1].bits & 0x1;
+            bb.bits = array[j+1].bits & (0xF >> i);
+            cc.bits = array[j+1].bits & ((~0xF) >> i);
+            xx.bits = aa.bits << (NSTAGES - 1 - i);
+            yy.bits = bb.bits >> 1;
 
-                r.bits = x.bits | y.bits | c.bits;
-                s.bits = xx.bits | yy.bits | cc.bits;
+            r.bits = x.bits | y.bits | c.bits;
+            s.bits = xx.bits | yy.bits | cc.bits;
 
 
-                array[j].bits = r.bits;
-                array[j+1].bits = s.bits;
-            //}
+            array[j].bits = r.bits;
+            array[j+1].bits = s.bits;
 
             // this first approach was intended to do what is actually
             // perform in the code above (see the README file), but, 
@@ -184,10 +197,7 @@ void fourier(COMPLEX_NUMBER data[], int nn, int isign)
 
             data[_v.bits].re = temprB;
             data[_v.bits].im = tempiB;
-
-            // swap
-            // TODO: implement a swap fuction
-
+            
 #ifdef _DEBUG_ 
             //cout << "Stage: " << i << endl;
             //cout << "_u.bits = j: " << j << endl;
@@ -207,9 +217,65 @@ void fourier(COMPLEX_NUMBER data[], int nn, int isign)
 
             //cout << "dataRe[" << _v.bits << "] = " << data[_v.bits].re << endl;
             //cout << "dataIm[" << _v.bits << "] = " << data[_v.bits].im << endl;
+            //
+            // alternate
+            //cout << "dataRe[" << r.bits << "] = " << data[r.bits].re << endl;
+            //cout << "dataIm[" << r.bits << "] = " << data[r.bits].im << endl;
+
+            //cout << "dataRe[" << s.bits << "] = " << data[s.bits].re << endl;
+            //cout << "dataIm[" << s.bits << "] = " << data[s.bits].im << endl;
 #endif
         }   // end for
     }   // end for
+
+    // swap
+    for (int jj = 1; jj < (NN / 4); ++jj)
+    {
+        temp1 = data[array[jj].bits].re;
+        temp2 = data[array[jj].bits].im;
+
+        data[array[jj].bits].re = data[jj].re;
+        data[array[jj].bits].im = data[jj].im;
+
+        data[jj].re = temp1;
+        data[jj].im = temp2;
+    }
+
+    for (int tt = NN / 4 + 1; tt < (NN / 2); ++tt)
+    {
+        temp1 = data[array[tt].bits].re;
+        temp2 = data[array[tt].bits].im;
+
+        data[array[tt].bits].re = data[tt].re;
+        data[array[tt].bits].im = data[tt].im;
+
+        data[tt].re = temp1;
+        data[tt].im = temp2;
+    }
+
+    for (int kk = NN / 2 + NN / 4 - 1; kk < (NN * 3 / 4); ++kk)
+    {
+        temp1 = data[array[kk].bits].re;
+        temp2 = data[array[kk].bits].im;
+
+        data[array[kk].bits].re = data[kk].re;
+        data[array[kk].bits].im = data[kk].im;
+
+        data[kk].re = temp1;
+        data[kk].im = temp2;
+    }
+
+#ifdef _DEBUG_ 
+    // print out the FFT
+    for (int qq = 0; qq < NN; ++qq)
+    {
+        cout << right << "dataRe[" << setw(4) << qq << "] = " 
+             << setw(8) << fixed << setprecision(4) << data[qq].re << "\t\t";
+
+        cout << right << "dataIm[" << setw(4) << qq << "] = " 
+             << setw(8) << fixed << setprecision(4) << data[qq].im << endl;
+    }
+#endif
 
 }   // end fourier
 
@@ -254,9 +320,42 @@ int main()
     // calculate FFT
 	fourier(arreglo, 16, 1);
 
+    arreglo[0].re = 45.0;
+    arreglo[0].im = 0.0;
+    arreglo[1].re = -25.4520;
+    arreglo[1].im = 16.6652;
+    arreglo[2].re = 10.3640;
+    arreglo[2].im = -3.2929;
+    arreglo[3].re = -9.0641;
+    arreglo[3].im = -2.3285;
+    arreglo[4].re = 4.0;
+    arreglo[4].im = 5.0;
+    arreglo[5].re = -1.2791;
+    arreglo[5].im = -5.6422;
+    arreglo[6].re = -2.3640;
+    arreglo[6].im = 4.7071;
+    arreglo[7].re = 3.7951;
+    arreglo[7].im = -2.6485;
+    arreglo[8].re = -5.0;
+    arreglo[8].im = 0;
+    arreglo[9].re = 3.7951;
+    arreglo[9].im = 2.6485;
+    arreglo[10].re = -2.3640;
+    arreglo[10].im = -4.7071;
+    arreglo[11].re = -1.2791;
+    arreglo[11].im = 5.6422;
+    arreglo[12].re = 4.0;
+    arreglo[12].im = -5.0;
+    arreglo[13].re = -9.0641;
+    arreglo[13].im = 2.3285;
+    arreglo[14].re = 10.3640;
+    arreglo[14].im = 3.2929;
+    arreglo[15].re = -25.4520;
+    arreglo[15].im = -16.6652;
+
     // calculate IFFT
-	//fourier(arreglo, 16, -1);
-    
+	fourier(arreglo, 16, -1);
+    // need to normalize    
 
     return 0;
 }   // end main
